a5bc758d5d7ba93338c1746e07706bf323c4eb8e
Chaw Chit <chawchitsuthwe3699@gmail.com>
2020-06-10 15:52:58 +0630

    Add addNewList function

--- js/index.js
+++ js/index.js
@@ -1,7 +1,8 @@
 const url = "http://localhost:8181/";
+var maxListPos;
 
 const addListBtn = `
-  	<button class="btn btn-sm btn-new-list text-left" data-toggle="modal" data-target="#add-list-modal">
+  	<button id="add-list-btn" class="btn btn-sm btn-new-list text-left" data-toggle="modal" data-target="#add-list-modal">
         <i class="fa fa-plus"></i>&nbsp;&nbsp;Add another list
 	</button>
   	<div style="width: 0.5rem">&nbsp;</div>
@@ -9,7 +10,7 @@ const addListBtn = `
 
 
 window.onload = () => {
-  	console.log("DOM is ready!");
+  	//console.log("DOM is ready!");
   	limitWrapperHeight();
   	fetchListData();
 };
@@ -24,20 +25,22 @@ function limitWrapperHeight() {
 }
 
 function fetchListData(){
-	fetch(url+"list")
-  		.then(function (response) {
-    		return response.json();
-  		})
-  		.then(function (data) {
-    		document.getElementById("wrapper").innerHTML = getList(data) + addListBtn;
-  		})
-  		.catch(function (err) {
-    		console.log(err);
-  		});
+	fetch(url+"list/positionAsc")
+  	.then(function (response) {
+   		return response.json();
+  	})
+  	.then(function (data) {
+    	document.getElementById("wrapper").innerHTML = getList(data) + addListBtn;
+  	})
+  	.catch(function (err) {
+    	console.log(err);
+  	});
 }
 
 function getList(list){
 	var listStr = "";
+	maxListPos = list.length;
+
 	for(var i=0; i<list.length; i++){
 
 		listStr = listStr + 
@@ -75,7 +78,7 @@ function getCard(cards){
     		</div>
       		<p>${cards[i].title}</p>
       		<div class="d-flex justify-content-end">
-  				${getMember(cards[i].accounts)}
+      			${getMember(cards[i].accounts)}
 			</div>
     	</div>
 		`;
@@ -126,7 +129,7 @@ function getMember(accounts){
 
 			memberStr = memberStr +
 			`
-			<div class="card-member m-1">${getInitials(accounts[i].name)}</div>
+			<div class="card-member mr-1">${getInitials(accounts[i].name)}</div>
 			`;
 		}
 	}
@@ -174,20 +177,49 @@ function getInitials(name){
 
 function cardClicked(listTitle, cardId){
 	fetch(url+"card/"+cardId)
-  		.then(function (response) {
-    		return response.json();
-  		})
-  		.then(function (card) {
+  	.then(function (response) {
+    	return response.json();
+  	})
+  	.then(function (card) {
 
-    		document.getElementById("card-title").innerHTML = card.title;
-  			document.getElementById("list-title").innerHTML = listTitle;
-  			document.getElementById("card-member").innerHTML = getMember(card.accounts);
-  			document.getElementById("card-label").innerHTML = getLabelWithName(card.labels)
-  			document.getElementById("card-desc").innerHTML = card.description;
-  			document.getElementById("card-checklist").innerHTML = getChecklist(card.checklists)
-  		})
-  		.catch(function (err) {
-    		console.log(err);
-  		});
+    	document.getElementById("card-title").innerHTML = card.title;
+  		document.getElementById("list-title").innerHTML = listTitle;
+  		document.getElementById("card-member").innerHTML = getMember(card.accounts);
+  		document.getElementById("card-label").innerHTML = getLabelWithName(card.labels)
+  		document.getElementById("card-desc").innerHTML = card.description;
+  		document.getElementById("card-checklist").innerHTML = getChecklist(card.checklists)
+  	})
+  	.catch(function (err) {
+   		console.log(err);
+  	});
+
+}
+
+function addNewList(){
+	const listTitle = document.getElementById("new-list-title").value;
+
+	fetch(url + "/list", {
+      	method: "POST",
+     	headers: {
+        	"Content-Type": "application/json"
+      	},
+      	body: JSON.stringify({
+        	title: listTitle,
+        	position: maxListPos + 1,
+        	status: 1
+      	})
+    	})
+	.then(function(res) {
+		res.json();
+	})
+	.then(function (data) {
+    	document.getElementById("new-list-title").value = "";
+    	$('#add-list-modal').modal('hide');
+     	window.onload();
+  
+  	})
+  	.catch(function (err) {
+    	console.log(err);
+  	});
 
 }
